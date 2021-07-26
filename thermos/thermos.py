0ad1e69da5058f75d8912d2de884becb7f839566
Karina Melgoza <kmelgoza08@gmail.com>
2021-07-26 11:44:53 -0700

    initial commit

---
+++ thermos/thermos.py
@@ -0,0 +1,18 @@
+from flask import Flask, app, render_template, url_for
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/add')
+def add():
+    return render_template('add.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
